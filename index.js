2a320a85a67a65cbe303eb05629ac90b70f51101
keshavsoft <nknnkumar@yahoo.com>
2026-07-15 23:30:32 +0530

    to test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import getLatestVersion from "./bin/core/getLatestVersion.js";
 const load = async () => {
     const v = getLatestVersion();
 
-    const module = await import(`./bin/${v}/AddTableName/index.js`);
+    const module = await import(`./bin/${v}/addHtmlId/index.js`);
 
     return await module.default;
 };
