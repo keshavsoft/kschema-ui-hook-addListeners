afc2df26e448212fe0268f7b994dcba5327421e5
keshavsoft <nknnkumar@yahoo.com>
2026-07-16 01:21:56 +0530

    to test

--- test/v2/addHtmlId/test.js
+++ test/v2/addHtmlId/test.js
@@ -1,11 +1,12 @@
+import path from "path";
 import index from "../../../index.js";
 
 const startFunc = () => {
     index({
         showLog: true,
-        toPath: process.cwd(),
+        toPath: path.join(process.cwd(), "test", "v2", "addHtmlId"),
         inActionName: "Html1",
-        inFolderName: "Fold1"
+        folderName: "Fold1"
     });
 };
 
